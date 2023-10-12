338d3e52f3a75e28ce07a8f9c8101217f6105331
MorgynKass <morgynkass@gmail.com>
2023-10-12 16:47:36 -0600

    finished part two

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+const awesomeFunction = (req, res) => {
+    res.send("Hello world!");
+};
+
+const tTech = (req, res) => {
+    res.send("Tooele Tech is awesome!");
+};
+
+module.exports = {awesomeFunction, tTech};
